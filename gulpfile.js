410b6f3262ac88569dbc37e1f02406ab61e9a25d
Artyom Trityak <art.trityak@gmail.com>
2014-10-27 12:15:48 +0200

    Gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,50 @@
 var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
     connect = require('gulp-connect'),
-    plumber = require('gulp-plumber');
+    plumber = require('gulp-plumber'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function () {
+    gulp.src('./static/scss/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./static/css'));
+});
 
 gulp.task('jshint', function() {
-  gulp.src([
-    './**/*.js',
-    '!./node_modules/**/*.js',
-    '!./static/assets/**/*.js',
-    '!./coverage/**/*.js']
-  )
-  .pipe(jshint())
-  .pipe(plumber())
-  .pipe(jshint.reporter());
+    gulp.src([
+        './**/*.js',
+        '!./node_modules/**/*',
+        '!./coverage/**/*'
+    ])
+    .pipe(jshint())
+    .pipe(plumber())
+    .pipe(jshint.reporter());
 });
 
 gulp.task('server', function() {
-  connect.server({
-    port: 3000
-  });
+    connect.server({
+        port: 3000
+    });
+});
+
+gulp.task('watch-sass', function() {
+    gulp.watch([
+        './**/*.scss',
+        '!./node_modules/**/*'
+    ], ['sass']);
+});
+
+gulp.task('watch-js', function() {
+    gulp.watch([
+        './**/*.js',
+        '!./node_modules/**/*',
+        '!./coverage/**/*'
+    ], ['jshint']);
 });
 
 gulp.task('test', ['jshint']);
 
-gulp.task('default', ['jshint', 'server']);
+gulp.task('default', ['jshint', 'sass', 'watch-sass', 'watch-js', 'server']);
