23e1d35d3593b41b7ac096d0b9e0383f0aa73057
Artyom Trityak <art.trityak@gmail.com>
2014-10-31 14:44:29 +0200

    Basic requirejs config

--- static/app/config.js
+++ static/app/config.js
@@ -2,7 +2,20 @@ requirejs.config({
   baseUrl: 'static/app',
   urlArgs: 'bust=' +  Date.now(),
   paths: {
-    
+    // Angular
+    'angular': 'bower_components/angular/angular',
+    'angular-bootstrap': 'bower_components/angular-bootstrap/ui-bootstrap-tpls',
+    'angular-mocks': 'bower_components/angular-mocks/angular-mocks',
+    'angular-route': 'bower_components/angular-route/angular-route',
+    'angular-resource': 'bower_components/angular-resource/angular-resource',
+
+    //Other
+    //TODO: add css
+    'fullcalendar': 'bower_components/fullcalendar/dist/fullcalendar',
+    'jquery': 'bower_components/jquery/dist/jquery',
+    'moment': 'bower_components/moment/moment',
+    //TODO: add css
+    'twitter-bootstrap': 'bower_components/bootstrap/dist/js/bootstrap'
   },
 
   config: {
@@ -12,7 +25,15 @@ requirejs.config({
   },
 
   shim: {
-    
+    //Angular
+    'angular-bootstrap': ['angular', 'twitter-bootstrap'],
+    'angular-mocks': ['angular'],
+    'angular-route': ['angular'],
+    'angular-resource': ['angular'],
+
+    //Other
+    'fullcalendar': ['jquery'],
+    'twitter-bootstrap': ['jquery']
   },
 
   packages: [
