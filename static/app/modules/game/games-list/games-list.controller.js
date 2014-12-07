afadb3f062dd82c1ef1088584ed9927b9ef339de
Artyom Trityak <art.trityak@gmail.com>
2014-12-07 21:01:04 +0200

    Games list

--- static/app/modules/game/games-list/games-list.controller.js
+++ static/app/modules/game/games-list/games-list.controller.js
@@ -1,11 +1,20 @@
 define(function(require) {
   'use strict';
   
-  GamesListController.$inject = ['$rootScope', 'dgGameService'];
+  GamesListController.$inject = ['$location', 'dgGameService'];
   return GamesListController;
 
-  function GamesListController($rootScope, dgGameService) {
+  function GamesListController($location, dgGameService) {
     var vm = this;
+    vm.data = {};
 
+    var searchParams = $location.search();
+
+    vm.data.search = searchParams.search;
+    vm.doSearch = doSearch;
+
+    function doSearch() {
+      $location.search({search: vm.data.search, page: 1});
+    }
   }
 });
