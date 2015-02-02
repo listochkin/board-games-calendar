5325a3e0be197d689ff8231bc245575cb2f96a1b
vadikace <vadikace@gmail.com>
2015-02-02 12:23:55 +0200

    wishlist#14 add server wishlist controller

---
+++ server/modules/wishlist/wishlist.controller.js
@@ -0,0 +1,10 @@
+/*jslint node: true */
+'use strict';
+
+var WishlistModel = require('./model');
+
+module.exports.getWishlist = getWishlist;
+
+function getWishlist(req, res) {
+  console.log('#getWishlist');
+}
