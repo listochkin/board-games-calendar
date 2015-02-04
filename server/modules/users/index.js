200da7e0f21abc7cebe3ee883fef3b862d41d842
Artyom Trityak <art.trityak@gmail.com>
2015-02-04 22:48:39 +0200

    Email confirm

--- server/modules/users/index.js
+++ server/modules/users/index.js
@@ -9,9 +9,12 @@ router.post('/facebook', controller.facebook);
 router.post('/google', controller.google);
 
 router.post('/login', controller.login);
+router.get('/verifyemail/:token', controller.verifyEmail);
 router.post('/', controller.register);
 router.get('/me', controller.decodeUserId, controller.me);
 router.put('/me', controller.ensureAuthenticated, controller.updateMe);
+
+router.get('/me-required', controller.ensureAuthenticated, controller.me);
 
 router.put('/:userId', controller.modifyUser);
 router.get('/:userId', controller.getUser);
